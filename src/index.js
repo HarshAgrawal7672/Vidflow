e7f698481ecf0a7bbb5d2812860de1f4c5741fe8
HarshAgrawal7672 <harshagrawal7895@gmail.com>
2025-04-01 19:45:56 +0530

    util code

--- src/index.js
+++ src/index.js
@@ -9,3 +9,12 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server running on port ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+    process.exit(1);
+})
